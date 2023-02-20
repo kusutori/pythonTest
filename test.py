4ba9bebc3ebd2d7f1c20cd22edd9cd5d8d99aea1
okyan123 <3181584445@qq.com>
2023-02-20 23:09:37 +0800

    杂项

--- test.py
+++ test.py
@@ -1,42 +1,38 @@
-# 制作一个脚本，我想传入一个mp3格式的音频文件，然后可以将它的音频转为通过c++播放的格式
-# 但是我不知道怎么做，我想用python来做，但是我不知道怎么做
-# 请问有人知道怎么做吗？
-
-import os
-import sys
-import wave
-import struct
-import numpy as np
-import matplotlib.pyplot as plt
+import math
 
 
-def wav2raw(wav_file, raw_file):
-    wav = wave.open(wav_file, 'rb')
-    raw = open(raw_file, 'wb')
-    nframes = wav.getnframes()
-    data = wav.readframes(nframes)
-    raw.write(data)
-    wav.close()
-    raw.close()
+class shape:
+    def __init__(self) -> None:
+        pass
+
+    def area(self):
+        return "这只是一个形状"
 
 
-def raw2wav(raw_file, wav_file):
-    raw = open(raw_file, 'rb')
-    wav = wave.open(wav_file, 'wb')
-    wav.setnchannels(1)
-    wav.setsampwidth(2)
-    wav.setframerate(8000)
-    data = raw.read()
-    wav.writeframes(data)
-    raw.close()
-    wav.close()
+class Circle(shape):
+    def __init__(self, x, y, radius) -> None:
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.radius = radius
+        print("Circle 对象已构建")
+
+    def area(self):
+        return math.pi*self.radius*self.radius
 
 
-def raw2txt(raw_file, txt_file):
-    raw = open(raw_file, 'rb')
-    txt = open(txt_file, 'w')
-    data = raw.read()
-    for i in range(len(data)):
-        txt.write(str(data[i]) + '\n')
-    raw.close()
-    txt.close()
+class Rectangle(shape):
+    def __init__(self, width, length) -> None:
+        super().__init__()
+        self.width = width
+        self.length = length
+        print("Rectangle 对象已构建")
+
+    def area(self):
+        return self.width*self.length
+
+
+c1 = Circle(0, 0, 2)
+print(c1.area())
+r1 = Rectangle(1, 4)
+print(r1.area())
